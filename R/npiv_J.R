74f78214c6e67049ea932ad4cf2a6dd247f6fd55
Jeffrey Racine <racinej@mcmaster.ca>
2022-03-10 20:52:13 -0500

    Working on npivJ

--- R/npiv_J.R
+++ R/npiv_J.R
@@ -11,7 +11,7 @@ npivJ <- function(Y,
                  K.w.segments=1,
                  J.x.degree=3,
                  J.x.segments=1,
-                 J.x.segments.set=c(2,4,8,16,32,64,128),
+                 J.x.segments.set=c(2,4,8,16,32)-1,
                  knots=c("uniform","quantiles"),
                  basis=c("tensor","additive","glp"),
                  check.is.fullrank=FALSE,
@@ -43,6 +43,17 @@ npivJ <- function(Y,
 
     ## See function npiv() in R code npiv.R for descriptions...
 
+    ## Generate set of J1 J2 combinations given input
+    ## J.x.segments.set. expand.grid() creates all possible
+    ## combinations, then we select those with J2 > J1 (remove
+    ## symmetric computations and those with J2=J1)
+
+    J1.J2 <- expand.grid(J.x.segments.set,J.x.segments.set)
+    J1.J2 <- subset(J1.J2,Var2 > Var1)
+    
+    J.x.J1.segments <- J1.J2[1,1]
+    J.x.J2.segments <- J1.J2[1,2]
+
     ## Tim precomputes and reuses basis functions... thet are
     ## computationally efficient so might not save much time, morover
     ## if parallelized could be a waste and even add overhead
@@ -69,17 +80,15 @@ npivJ <- function(Y,
         Psi.x.J1.eval <- Psi.x.J1 <- matrix(1,NROW(X),1)
         Psi.x.J2.eval <- Psi.x.J2 <- matrix(1,NROW(X),1)
     } else {
-        Psi.x.J1 <- prod.spline(x=X,
+        Psi.x.J1.eval <- Psi.x.J1 <- prod.spline(x=X,
                             K=cbind(rep(J.x.degree,NCOL(X)),rep(J.x.J1.segments,NCOL(X))),
                             knots=knots,
                             basis=basis)
-        Psi.x.J2 <- prod.spline(x=X,
+        Psi.x.J2.eval <- Psi.x.J2 <- prod.spline(x=X,
                             K=cbind(rep(J.x.degree,NCOL(X)),rep(J.x.J2.segments,NCOL(X))),
                             knots=knots,
                             basis=basis)
 
-        if(basis!="tensor") Psi.x.J1 <- cbind(1,Psi.x.J1)
-        if(basis!="tensor") Psi.x.J2 <- cbind(1,Psi.x.J2)
         if(!is.null(X.eval)) {
             Psi.x.J1.eval <- prod.spline(x=X,
                                       xeval=X.eval,
@@ -92,20 +101,28 @@ npivJ <- function(Y,
                                       knots=knots,
                                       basis=basis)
 
-           if(basis!="tensor") Psi.x.J1.eval <- cbind(1,Psi.x.J1.eval)
-           if(basis!="tensor") Psi.x.J2.eval <- cbind(1,Psi.x.J2.eval)
         }
+        
+        if(basis!="tensor") {
+          Psi.x.J1 <- cbind(1,Psi.x.J1)
+          Psi.x.J2 <- cbind(1,Psi.x.J2)
+          Psi.x.J1.eval <- cbind(1,Psi.x.J1.eval)
+          Psi.x.J2.eval <- cbind(1,Psi.x.J2.eval)
+        }
+      
     }
 
-    BTB.inv <- chol2inv(chol(t(B.w)%*%B.w,pivot=chol.pivot))
+    B.w.TB.w.inv <- chol2inv(chol(t(B.w)%*%B.w,pivot=chol.pivot))
 
-    Psi.xJ1TB.wB.wTB.w.invB.w <- t(Psi.x.J1)%*%B.w%*%BTB.inv%*%t(B.w)
+    Psi.xJ1TB.wB.wTB.w.invB.w <- t(Psi.x.J1)%*%B.w%*%B.w.TB.w.inv%*%t(B.w)
     beta.J1 <- chol2inv(chol(Psi.xJ1TB.wB.wTB.w.invB.w%*%Psi.x.J1+diag(lambda,NCOL(Psi.x.J1)),pivot=chol.pivot))%*%Psi.xJ1TB.wB.wTB.w.invB.w%*%Y
     U.J1 <- Y-Psi.x.J1%*%beta.J1
+    err.J1 <- as.numeric(Psi.x.J1%*%chol2inv(chol(Psi.xJ1TB.wB.wTB.w.invB.w%*%Psi.x.J1+diag(lambda,NCOL(Psi.x.J1)),pivot=chol.pivot))%*%Psi.xJ1TB.wB.wTB.w.invB.w%*%U.J1)
 
-    Psi.xJ2TB.wB.wTB.w.invB.w <- t(Psi.x.J2)%*%B.w%*%BTB.inv%*%t(B.w)
+    Psi.xJ2TB.wB.wTB.w.invB.w <- t(Psi.x.J2)%*%B.w%*%B.w.TB.w.inv%*%t(B.w)
     beta.J2 <- chol2inv(chol(Psi.xJ2TB.wB.wTB.w.invB.w%*%Psi.x.J2+diag(lambda,NCOL(Psi.x.J2)),pivot=chol.pivot))%*%Psi.xJ2TB.wB.wTB.w.invB.w%*%Y
     U.J2 <- Y-Psi.x.J2%*%beta.J2
+    err.J2 <- as.numeric(Psi.x.J2%*%chol2inv(chol(Psi.xJ2TB.wB.wTB.w.invB.w%*%Psi.x.J2+diag(lambda,NCOL(Psi.x.J2)),pivot=chol.pivot))%*%Psi.xJ2TB.wB.wTB.w.invB.w%*%U.J2)
 
     ## Compute asymptotic variances and covariances for the IV
     ## functions - these will be memory intensive for large n as they
@@ -113,40 +130,32 @@ npivJ <- function(Y,
     ## around but simply note that as it stands the computation of the
     ## standard errors by brute force needs to be addressed)
 
-    CJ1 <- (t(Psi.x.J1)%*%B.w)
+    CJ1 <- t(Psi.x.J1)%*%B.w
     B.wUJ1 <- B.w*as.numeric(U.J1)
-    rho <- CJ1%*%BTB.inv%*%t(B.wUJ1)%*%(B.wUJ1)%*%BTB.inv%*%t(CJ1)
-    D.J1.inv <- chol2inv(chol(CJ1%*%BTB.inv%*%t(CJ1),pivot=chol.pivot))
+    rho <- CJ1%*%B.w.TB.w.inv%*%t(B.wUJ1)%*%(B.wUJ1)%*%B.w.TB.w.inv%*%t(CJ1)
+    D.J1.inv <- chol2inv(chol(CJ1%*%B.w.TB.w.inv%*%t(CJ1),pivot=chol.pivot))
     D.J1.inv.rho.D.J1.inv <- D.J1.inv%*%rho%*%D.J1.inv
+
     asy.var.J1 <- diag(Psi.x.J1.eval%*%D.J1.inv.rho.D.J1.inv%*%t(Psi.x.J1.eval))
 
-    CJ2 <- (t(Psi.x.J2)%*%B.w)
+    CJ2 <- t(Psi.x.J2)%*%B.w
     B.wUJ2 <- B.w*as.numeric(U.J2)
-    rho <- CJ2%*%BTB.inv%*%t(B.wUJ2)%*%(B.wUJ2)%*%BTB.inv%*%t(CJ2)
-    D.J2.inv <- chol2inv(chol(CJ2%*%BTB.inv%*%t(CJ2),pivot=chol.pivot))
+    rho <- CJ2%*%B.w.TB.w.inv%*%t(B.wUJ2)%*%(B.wUJ2)%*%B.w.TB.w.inv%*%t(CJ2)
+    D.J2.inv <- chol2inv(chol(CJ2%*%B.w.TB.w.inv%*%t(CJ2),pivot=chol.pivot))
     D.J2.inv.rho.D.J2.inv <- D.J2.inv%*%rho%*%D.J2.inv
     asy.var.J2 <- diag(Psi.x.J2.eval%*%D.J2.inv.rho.D.J2.inv%*%t(Psi.x.J2.eval))
 
     ## Compute the covariance
 
-    cov.J1.J2 <- diag(Psi.x.J1.eval%*%D.J1.inv%*%CJ1%*%BTB.inv%*%t(B.wUJ1)%*%(B.wUJ2)%*%BTB.inv%*%t(CJ2)%*%D.J2.inv%*%Psi.x.J2.eval)
+    cov.J1.J2 <- diag(Psi.x.J1.eval%*%D.J1.inv%*%CJ1%*%B.w.TB.w.inv%*%t(B.wUJ1)%*%(B.wUJ2)%*%B.w.TB.w.inv%*%t(CJ2)%*%t(D.J2.inv)%*%t(Psi.x.J2.eval))
+    asy.se <- as.numeric(sqrt(asy.var.J1+asy.var.J2-2*cov.J1.J2))
 
-    asy.se <- sqrt(asy.var.J1+asy.var.J2-2*cov.J1.J2)
+    Z <- (err.J1-err.J2)/asy.se
 
     ## Return a list with various objects that might be of interest to
     ## the user
 
-    return(list(h=h,
-                h.asy.se=asy.se,
-                K.w.degree=K.w.degree,
-                K.w.segments=K.w.segments,
-                J.x.degree=J.x.degree,
-                J.x.segments=J.x.segments,
-                beta=beta,
-                B.w=B.w,
-                Psi.x=Psi.x,
-                residuals.sample=Y-Psi.x%*%beta,
-                residuals.eval=Y-Psi.x.eval%*%beta)
+    return(list(J=NULL))
 
 }
 
