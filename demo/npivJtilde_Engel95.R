e09272c65f2a426b51523054be1eb5af3147360e
Jeffrey Racine <racinej@mcmaster.ca>
2022-03-14 20:40:14 -0400

    Added Engel95 with J.tilde complexity choice to demo

---
+++ demo/npivJtilde_Engel95.R
@@ -0,0 +1,50 @@
+data(Engel95)
+
+## Sort on logexp (the endogenous regressor) for plotting purposes
+
+Engel95 <- Engel95[order(Engel95$logexp),] 
+
+attach(Engel95)
+
+out.choose.J <- npiv_choose_J(food,
+                              logexp,
+                              logwages)
+    
+model.iv <- npiv(food,
+                 logexp,
+                 logwages,
+                 K.w.segments=out.choose.J$K.w.seg,
+                 J.x.segments=out.choose.J$J.x.seg)
+
+model.niv <- npiv(food,
+                  logexp,
+                  logexp,
+                  K.w.segments=out.choose.J$K.w.seg,
+                  J.x.segments=out.choose.J$J.x.seg)
+
+## For the plots, restrict focal attention to the bulk of the data
+## (i.e. for the plotting area trim out 1/4 of one percent from each
+## tail of Y and X)
+
+trim <- 0.0025
+
+plot(logexp,food,
+     ylab="Food Budget Share",
+     xlab="log(Total Expenditure)",
+     xlim=quantile(logexp,c(trim,1-trim)),
+     ylim=quantile(food,c(trim,1-trim)),
+     main="Nonparametric Instrumental Variables",
+     type="p",
+     cex=.5,
+     col="lightgrey")
+
+lines(logexp,model.iv$fitted,col="blue",lwd=2,lty=2)
+
+lines(logexp,model.niv$fitted,col="red",lwd=2,lty=4)
+
+legend("topright",
+       c("Nonparametric IV","Nonparametric Regression"),
+       lty=c(2,4),
+       col=c("blue","red"),
+       lwd=c(2,2),
+       bty="n")
