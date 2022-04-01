f4d11e0a4850c72315d132216f26eb2d8c3da8f1
Jeffrey Racine <racinej@mcmaster.ca>
2022-04-01 12:57:43 -0400

    Minor edits, removing unnecessary functionality

--- R/mgcv.R
+++ R/mgcv.R
@@ -32,19 +32,3 @@ tensor.prod.model.matrix <- function(X) {
   class(T) <- "matrix"
   T
 } ## end tensor.prod.model.matrix
-
-uniquecombs<-function(x) {
-## takes matrix x and counts up unique rows
-## `unique' now does this in R
-if (is.null(x)) stop("x is null")
-if (is.null(nrow(x))) stop("x has no row attribute")
-if (is.null(ncol(x))) stop("x has no col attribute")
-ind <- rep(0,nrow(x))
-res<-.C("RuniqueCombs",x=as.double(x),ind=as.integer(ind),
-        r=as.integer(nrow(x)),c=as.integer(ncol(x)))
-n <- res$r*res$c
-x <- matrix(res$x[1:n],res$r,res$c)
-attr(x,"index") <- res$ind+1 ## C to R index gotcha
-x
-}
-
