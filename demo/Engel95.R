5e422bc13bd88b7c739deea59608f2989fbaec02
Tim Christensen <tmc8@nyu.edu>
2022-04-07 09:13:46 -0400

    Update Engel curve demo

--- demo/Engel95.R
+++ demo/Engel95.R
@@ -1,42 +1,55 @@
-data(Engel95)
+## load data
+data("Engel95", package = "npiv")
 
-## Sort on logexp (the endogenous regressor) for plotting purposes
-
+## sort on logexp (the regressor) for plotting purposes
 Engel95 <- Engel95[order(Engel95$logexp),] 
-
 attach(Engel95)
 
-model.iv <- npiv(food,
-                 logexp,
-                 logwages)
+## Estimate the Engel curve for food using logwages as an instrument
+fm1 <- npiv(food ~ logexp | logwages)
 
-model.niv <- npiv(food,
-                  logexp,
-                  logexp)
-
-## For the plots, restrict focal attention to the bulk of the data
-## (i.e. for the plotting area trim out 1/4 of one percent from each
-## tail of Y and X)
-
-trim <- 0.025
-
+## Plot the estimated Engel curve and data-driven uniform confidence bands
 plot(logexp,food,
      ylab="Food Budget Share",
-     xlab="log(Total Expenditure)",
-     xlim=quantile(logexp,c(trim,1-trim)),
-     ylim=quantile(food,c(trim,1-trim)),
-     main="Nonparametric Instrumental Variables",
+     xlab="log(Total Household Expenditure)",
+     xlim=c(4.75, 6.25),
+     ylim=c(0, 0.4),
+     main="",
      type="p",
      cex=.5,
      col="lightgrey")
-
-lines(logexp,model.iv$h,col="blue",lwd=2,lty=2)
-
-lines(logexp,model.niv$h,col="red",lwd=2,lty=4)
+lines(logexp,fm1$h,col="blue",lwd=2,lty=1)
+lines(logexp,fm1$h.upper,col="blue",lwd=2,lty=2)
+lines(logexp,fm1$h.lower,col="blue",lwd=2,lty=2)
 
 legend("topright",
-       c("Nonparametric IV","Nonparametric Regression"),
-       lty=c(2,4),
-       col=c("blue","red"),
-       lwd=c(2,2),
-       bty="n")
+       legend=c("Estimate",
+                "95 per cent UCBs"),
+       col=c("blue","blue"),
+       lty=c(1,2),
+       lwd=c(2,2))
+
+
+## Compare with the Engel curve for leisure
+fm2 <- npiv(leisure ~ logexp | logwages)
+
+## Plot the estimated Engel curve and data-driven uniform confidence bands
+plot(logexp,leisure,
+     ylab="Leisure Budget Share",
+     xlab="log(Total Household Expenditure)",
+     xlim=c(4.75, 6.25),
+     ylim=c(0, 0.4),
+     main="",
+     type="p",
+     cex=.5,
+     col="lightgrey")
+lines(logexp,fm2$h,col="blue",lwd=2,lty=1)
+lines(logexp,fm2$h.upper,col="blue",lwd=2,lty=2)
+lines(logexp,fm2$h.lower,col="blue",lwd=2,lty=2)
+
+legend("topright",
+       legend=c("Estimate",
+                "95 per cent UCBs"),
+       col=c("blue","blue"),
+       lty=c(1,2),
+       lwd=c(2,2))
