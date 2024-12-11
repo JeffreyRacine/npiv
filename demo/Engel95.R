47e06712855f026da3c4e9748fd1e29c97201f8c
Tim Christensen <timothy.m.christensen@gmail.com>
2024-12-10 20:00:01 -0500

    Update Engel curve example with S3 plots

--- demo/Engel95.R
+++ demo/Engel95.R
@@ -1,55 +1,29 @@
-## load data
+## Empirical illustration: Engel curve estimation
+
+library(npiv)
+
+## Load data
 data("Engel95", package = "npiv")
 
-## sort on logexp (the regressor) for plotting purposes
+## Sort on logexp (the regressor) for plotting purposes
 Engel95 <- Engel95[order(Engel95$logexp),] 
 attach(Engel95)
+logexp.eval <- seq(4.5,6.5,length=100)
 
 ## Estimate the Engel curve for food using logwages as an instrument
-fm1 <- npiv(food ~ logexp | logwages)
+food_engel <- npiv(food, logexp, logwages, X.eval = logexp.eval)
 
-## Plot the estimated Engel curve and data-driven uniform confidence bands
-plot(logexp,food,
-     ylab="Food Budget Share",
-     xlab="log(Total Household Expenditure)",
-     xlim=c(4.75, 6.25),
-     ylim=c(0, 0.4),
-     main="",
-     type="p",
-     cex=.5,
-     col="lightgrey")
-lines(logexp,fm1$h,col="blue",lwd=2,lty=1)
-lines(logexp,fm1$h.upper,col="blue",lwd=2,lty=2)
-lines(logexp,fm1$h.lower,col="blue",lwd=2,lty=2)
+## Plot the estimated function and uniform confidence bands
+plot(food_engel, showdata = TRUE)
 
-legend("topright",
-       legend=c("Estimate",
-                "95 per cent UCBs"),
-       col=c("blue","blue"),
-       lty=c(1,2),
-       lwd=c(2,2))
+## Repeat for leisure
+leisure_engel <- npiv(leisure, logexp, logwages, X.eval = logexp.eval)
+plot(leisure_engel, showdata = TRUE)
 
+## Estimate the Engel curve for food by regression using the default (knots uniformly spaced)
+food_engel_reg_uniform <- npiv(food, logexp, logexp, X.eval = logexp.eval)
+plot(food_engel_reg_uniform, showdata = TRUE)
 
-## Compare with the Engel curve for leisure
-fm2 <- npiv(leisure ~ logexp | logwages)
-
-## Plot the estimated Engel curve and data-driven uniform confidence bands
-plot(logexp,leisure,
-     ylab="Leisure Budget Share",
-     xlab="log(Total Household Expenditure)",
-     xlim=c(4.75, 6.25),
-     ylim=c(0, 0.4),
-     main="",
-     type="p",
-     cex=.5,
-     col="lightgrey")
-lines(logexp,fm2$h,col="blue",lwd=2,lty=1)
-lines(logexp,fm2$h.upper,col="blue",lwd=2,lty=2)
-lines(logexp,fm2$h.lower,col="blue",lwd=2,lty=2)
-
-legend("topright",
-       legend=c("Estimate",
-                "95 per cent UCBs"),
-       col=c("blue","blue"),
-       lty=c(1,2),
-       lwd=c(2,2))
+## Repeat with knots at quantiles
+food_engel_reg_quantiles <- npiv(food, logexp, logexp, X.eval = logexp.eval, knots = "quantiles")
+plot(food_engel_reg_quantiles, showdata = TRUE)
